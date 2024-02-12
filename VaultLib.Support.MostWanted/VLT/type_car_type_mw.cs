bc3fa1de4ea298ab0ef70baf73c05667cb5f8138
Alperen Aydın <108737692+rybetasz@users.noreply.github.com>
2024-02-12 10:06:06 +0300

    Update type_car_type_mw.cs

--- VaultLib.Support.MostWanted/VLT/type_car_type_mw.cs
+++ VaultLib.Support.MostWanted/VLT/type_car_type_mw.cs
@@ -1,1 +1,14 @@
-using 
+﻿using VaultLib.Core.Types;
+
+namespace VaultLib.Frameworks.Speed.Csis
+{
+    [VLTTypeInfo("Csis::Type_car_type")]
+    public enum Type_car_type
+    {
+
+
+
+
+    }
+
+}
